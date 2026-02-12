c7e1be8d49c21f8ea4b590646e92ecd744946cc3
Brad Breiten <brad.breiten@housecallpro.com>
2026-02-12 13:51:42 -0700

    Update schema to remove users table and add decks and cards

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,24 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const decksTable = pgTable("decks", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  ownerId: varchar({ length: 255 }).notNull(), // Clerk user ID
+  title: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+});
+
+export const cardsTable = pgTable("cards", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  deckId: integer()
+    .notNull()
+    .references(() => decksTable.id, { onDelete: "cascade" }),
+  front: text().notNull(),
+  back: text().notNull(),
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
 });
